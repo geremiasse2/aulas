a901f9265cacf136a570067374f0ab8f166e7aaa
Geremias Soares <geremiasse@hotmail.com.br>
2024-04-24 21:59:07 -0300

    nota

--- exercicios/tabuada.py
+++ exercicios/tabuada.py
@@ -1,6 +1,6 @@
 numero = int(input('Digite um número entre 1 e 10 para gerar uma tabuada: '))
 
-while numero < 1 or numero > 10:
+while 1 > numero > 10:
     numero = input('Número inválido. Insira um número entre 1 e 10 ')
 
 
