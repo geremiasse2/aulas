27292e7f92919d7fd372c2b1df1e76044df842b8
Geremias Soares <geremiasse@hotmail.com.br>
2024-05-01 22:47:00 -0300

    Exercício cadastro de clientes

--- cadastro_clientes/clientes.py
+++ cadastro_clientes/clientes.py
@@ -2,13 +2,16 @@ clientes = []
 
 
 def cadastro_clientes():
-    clientes.append({'nome': input('Informe o nome do cliente: '), 'telefone': input('Informe o telefone do cliente: '), 'email': input('Informe o email do cliente:')})
+    clientes.append({'nome': input('Informe o nome do cliente: '), 'telefone': input('Informe o telefone do cliente: '),
+                     'email': input('Informe o email do cliente: '), 'disponivel': False})
     print('Cadastro realizado com sucesso!')
 
 
 def listar_clientes():
     for index, cliente in enumerate(clientes):
-        print(f'{index+1} - {cliente["nome"]} - {cliente["telefone"]} - {cliente["email"]}') #pedir para explicar essa linha
+        #status = '✓' if cadastro['disponivel'] else ' ' (ver por que tá dando erro)
+        print(
+            f'{index + 1} - {cliente["nome"]} - {cliente["telefone"]} - {cliente["email"]} ')  #pedir para explicar essa linha
 
 
 def editar_cliente(indice):
@@ -16,6 +19,18 @@ def editar_cliente(indice):
     clientes[indice]['telefone'] = input('Informe o novo telefone do cliente: ')
     clientes[indice]['email'] = input('Informe o novo email do cliente: ')
     print('Cadastro alterado com sucesso!')
+
+
+def disponivel(indice):
+    clientes[indice][disponivel] = True
+
+
+def excluir_cliente():
+    for cliente in clientes:
+        print(int(input('Qual cliente deseja excluir?')))
+        clientes.remove(cliente)
+#print('Cliente removido com sucesso ')
+
 
 while True:
     cadastro = input('''
@@ -36,4 +51,13 @@ while True:
             listar_clientes()
         case '3':
             cadastro = int(input('Qual cliente deseja editar? '))
-            editar_cliente(cadastro-1)
+            editar_cliente(cadastro - 1)
+        case '4':
+            cadastro = (input('Qual cliente deseja marcar como disponível?'))
+            listar_clientes(cadastro - 1)
+        case '6':
+            excluir_cliente()
+        case '7':
+            break
+        case _:
+            print('Opção inválida! ')
